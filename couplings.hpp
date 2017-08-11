68d2451611c29414d971c2b511bf57cc13712196
Nathan Carlson <nathan.l.carlson@und.edu>
2017-08-11 15:52:32 -0500

    Implement new format for general couplings
    This format allows users to define a coupling energy function and pass it as a parameter to the constructor.
    The form of the energy function has position, x, y, z, and, for now, a spin attribute for each 'node', or 'grain', or 'particle' etc...
    Additionally this format for couplings implements a common coupling, square with neighbors being up, down, left, and, right. It allows for periodic or non-periodic boundary conditions.
    Removes couple and decouple functions as they somewhat repeat the functionality of the overloaded operator.

--- couplings.hpp
+++ couplings.hpp
@@ -2,29 +2,77 @@
 #define COUPLINGS_HPP
 
 #include <map>
+#include <functional>
+#include <tuple>
 #include "utils.hpp"
 
 namespace couplings{
-
+  typedef std::function<double( utils::node, utils::node )> EnergyFunction_Ptr;
   class GeneralCouplings {
     public:
-      GeneralCouplings(int t_n)
-        : m_size(t_n)
+      GeneralCouplings(int t_n, EnergyFunction_Ptr f)
+        : m_size(t_n), energy(f)
       {
       }
-      void couple(const int i, const int j, const double h){
-        m_map.insert(std::make_pair(std::make_pair(i,j), h));
-        m_map.insert(std::make_pair(std::make_pair(j,i), h));
-      }
-      void decouple(const int i, const int j){
-        m_map.erase(std::make_pair(i,j));
-        m_map.erase(std::make_pair(j,i));
-      }
+
       bool are_coupled(const int i, const int j){
         return m_map.find(std::make_pair(i,j)) != m_map.end();
       }
       double &operator() (int i, int j){
         return m_map[std::make_pair(i,j)];
+      }
+      void square2D( bool periodic = true ){
+        int w = std::sqrt(m_size);
+        int j;
+        int k;
+        int x;
+        int y;
+        utils::node inode;
+        utils::node jnode;
+        if (periodic) {
+          for(int i = 0; i < m_size; i++){
+            x = i%w;
+            y = i/w;
+            j = w*y + utils::mod( i+1, w );
+            m_map[std::make_pair(i,j)] = energy(
+              utils::make_node(x, y, 0, 0), utils::make_node(j%w, j/w, 0, 0));
+            j = w*(y) + utils::mod( i-1, w );
+            m_map[std::make_pair(i,j)] = energy(
+              utils::make_node(x, y, 0, 0), utils::make_node(j%w, j/w, 0, 0));
+            j = utils::mod( i+w, m_size );
+            m_map[std::make_pair(i,j)] = energy(
+              utils::make_node(x, y, 0, 0), utils::make_node(j%w, j/w, 0, 0));
+            j = utils::mod( i-w, m_size );
+            m_map[std::make_pair(i,j)] = energy(
+              utils::make_node(x, y, 0, 0), utils::make_node(j%w, j/w, 0, 0));
+          }
+        }
+        else{
+          for(int i = 0; i < m_size; i++){
+            x = i%w;
+            y = i/w;
+            if( (i+1)%w != 0 ){
+              j = w*y+utils::mod( i+1, w);
+              m_map[std::make_pair(i,j)] = energy(
+                utils::make_node(x, y, 0, 0), utils::make_node(j%w, j/w, 0, 0));
+            }
+            if( x != 0 ){
+              j = w*y+utils::mod( i-1, w);
+              m_map[std::make_pair(i,j)] = energy(
+                utils::make_node(x, y, 0, 0), utils::make_node(j%w, j/w, 0, 0));
+            }
+            if( i+w < m_size ){
+              j = utils::mod( i+w, m_size);
+              m_map[std::make_pair(i,j)] = energy(
+                utils::make_node(x, y, 0, 0), utils::make_node(j%w, j/w, 0, 0));
+            }
+            if( i-w >= 0 ){
+              j = utils::mod( i-w, m_size);
+              m_map[std::make_pair(i,j)] = energy(
+                utils::make_node(x, y, 0, 0), utils::make_node(j%w, j/w, 0, 0));
+            }
+          }
+        }
       }
       void print(){
         std::map<std::pair<int, int>, double>::iterator it = m_map.begin();
@@ -32,39 +80,10 @@ namespace couplings{
         for (it=m_map.begin(); it!=m_map.end(); ++it)
           std::cout << "(" << it->first.first << ", " << it->first.second << ")" << " => " << it->second << '\n';
       }
-    protected:
+    private:
+      EnergyFunction_Ptr energy;
       int m_size;
       std::map<std::pair<int, int>, double> m_map;
-  };
-  class SquareCouplings: public GeneralCouplings {
-    public:
-      SquareCouplings(int t_n, const double h, bool periodic = true) : GeneralCouplings(t_n){
-        int w = std::sqrt(m_size);
-        if (periodic) {
-          for(int i = 0; i < m_size; i++){
-            this->couple(i, w*(i/w)+utils::mod( i+1, w), h);
-            this->couple(i, w*(i/w)+utils::mod( i-1, w), h);
-            this->couple(i, utils::mod( i+w, m_size), h);
-            this->couple(i, utils::mod( i-w, m_size), h);
-          }
-        }
-        else{
-          for(int i = 0; i < m_size; i++){
-            if( (i+1)%w != 0 ){
-              this->couple(i, w*(i/w)+utils::mod( i+1, w), h);
-            }
-            if( i%w != 0 ){
-              this->couple(i, w*(i/w)+utils::mod( i-1, w), h);
-            }
-            if( i+w < m_size ){
-              this->couple(i, utils::mod( i+w, m_size), h);
-            }
-            if( i-w > 0 ){
-              this->couple(i, utils::mod( i-w, m_size), h);
-            }
-          }
-        }
-      }
   };
 }
 #endif
