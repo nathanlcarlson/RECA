1d9204cd22b5f18638e150bd8246d03650416b71
Nathan Carlson <nathan.l.carlson@und.edu>
2017-08-26 12:43:45 -0500

    Implement RECA using Algorithm interface

--- algorithm.hpp
+++ algorithm.hpp
@@ -7,14 +7,22 @@ class Algorithm {
   public:
     virtual ~Algorithm();
     virtual void evolve_state();
-    virtual void setState();
+    virtual void setState(State* t_state);
 };
 class RECA : public Algorithm {
   public:
-    RECA(State* t_state);
+    RECA();
+    virtual void evolve_state();
+    virtual void setState(State* t_state);
   private:
     State* m_state;
     std::vector< double > m_replica;
     std::vector< int > m_traversed;
+    int m_N;
+    int m_w;
+    bool in_m_traversed(int i);
+    void swap(int j);
+    void q_swap(int i , int j);
+    void propigate(int);
 };
 #endif
