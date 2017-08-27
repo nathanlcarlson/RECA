560860a4ccf80cdbdf9c2718fd786e5cba136a15
Nathan Carlson <nathan.l.carlson@und.edu>
2017-08-27 13:48:52 -0500

    Clean up and add some comments

--- main.cpp
+++ main.cpp
@@ -14,12 +14,14 @@ double energy(int i, int j);
 double rotate_y=0;
 double rotate_x=0;
 
+// The width of our 2D square and total number of nodes
 int n = 1<<8;
 int n_nodes = n*n;
+// Display parameters
 double size = 0.8;
 double w = size/n;
+// Physical parameter of system
 double beta = 30.0;
-
 
 StaticCouplings2D A(n_nodes, a_coulping_energy);
 StaticCouplings2D J(n_nodes, j_coulping_energy);
@@ -29,12 +31,12 @@ Metropolis* my_metro = new Metropolis();
 //State<RECA> my_state(n_nodes, beta ,energy, my_reca);
 State<Metropolis> my_state(n_nodes, beta ,energy, my_metro);
 
-double a_coulping_energy(node i,
-                         node j){
+double a_coulping_energy(node i, node j){
+
   return (i.x * j.y - i.y * j.x)/(double)n;
 }
-double j_coulping_energy(node i,
-                         node j){
+double j_coulping_energy(node i, node j){
+
   return 1.0;
 }
 double energy(int i, int j) {
@@ -71,22 +73,16 @@ void display_state(void) {
 void specialKeys(GLFWwindow* window, int key, int scancode, int action, int mods) {    //The current mouse coordinates)
   if (key == GLFW_KEY_ESCAPE )
     exit(0);
-
   else if (key == GLFW_KEY_RIGHT)
     my_state.m_B += 0.1;
-
   else if (key == GLFW_KEY_LEFT)
     my_state.m_B -= 0.1;
-
   else if (key == GLFW_KEY_UP)
     rotate_x += 5;
-
   else if (key == GLFW_KEY_DOWN)
     rotate_x -= 5;
-
 }
 
-//Main program
 int main(int argc, char **argv) {
 
   //For outliving declared scope
@@ -101,40 +97,38 @@ int main(int argc, char **argv) {
 
   GLFWwindow* window;
 
-  /* Initialize the library */
+  // Initialize the library
   if (!glfwInit())
       return -1;
 
-  /* Create a windowed mode window and its OpenGL context */
+  // Create a windowed mode window and its OpenGL context
   window = glfwCreateWindow(640, 640, "State", NULL, NULL);
   if (!window)
   {
       glfwTerminate();
       return -1;
   }
-  //glfwSwapInterval(1);
-  /* Make the window's context current */
+  // Make the window's context current
   glfwMakeContextCurrent(window);
   glfwSetKeyCallback(window, specialKeys);
-  /* Loop until the user closes the window */
+
+  // Loop until the user closes the window
+  // Only render every 2^n steps
   int count = 0;
-  int n_steps = 1 << 12;
+  int n_steps = 1 << 9;
   while (!glfwWindowShouldClose(window))
   {
-      /* Render here */
+      // Step the state forward
       my_state.evolve_state();
+
       count++;
       if (count == n_steps){
         display_state();
+        // Swap front and back buffers
         glfwSwapBuffers(window);
         count = 0;
       }
-
-
-      //std::cout << my_state.m_B << std::endl;
-      /* Swap front and back buffers */
-
-      /* Poll for and process events */
+      // Poll for and process events
       glfwPollEvents();
   }
 
