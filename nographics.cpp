92f4135d3471b517148ad7760841dc8ad32e11db
Nathan Carlson <carlsonfatcat0@gmail.com>
2017-09-21 12:30:15 -0500

    Start a no graphics option

---
+++ nographics.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <math.h>
+#include "utils.hpp"
+#include "couplings.hpp"
+#include "state.hpp"
+#include "algorithm.hpp"
+
+
+double a_coulping_energy(node i, node j);
+double j_coulping_energy(node i, node j);
+double energy(int i, int j);
+
+// The width of our 2D square and total number of nodes
+int n = 1 << 8;
+int n_nodes = n * n;
+double N_states = 200.0;
+// Physical parameter of system
+double beta = 30.0;
+
+// Couplings used to calculate energy
+StaticCouplings2D A(n_nodes, a_coulping_energy);
+StaticCouplings2D J(n_nodes, j_coulping_energy);
+State my_state(n_nodes, beta, energy, N_states);
+
+// Choices of algorithms, use info from couplings
+RECA <StaticCouplings2D> *my_reca = new RECA <StaticCouplings2D>( &my_state, &A );
+Metropolis <StaticCouplings2D> *my_metro = new Metropolis <StaticCouplings2D>( &my_state, &A );
+
+double a_coulping_energy(node i, node j)
+{
+	return (i.x * j.y - i.y * j.x) / (double)n;
+}
+
+double j_coulping_energy(node i, node j)
+{
+	return 1.0;
+}
+
+double energy(int i, int j)
+{
+	return J(i, j) * cos(2 * M_PI * (my_state[i] - my_state[j] - A(i, j)));
+}
+
+int main(int argc, char **argv)
+{
+	// Set up couplings
+	A.square2D(false);
+	J.square2D(false);
+
+
+	while (true)
+	{
+		// Step the state forward
+   		my_metro->evolve_state();
+
+	}
+
+	return 0;
+}
