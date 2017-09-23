78a9ee1fc24ef626ad8b5a82e384a04f91355748
Nathan Carlson <nathan.l.carlson@und.edu>
2017-09-23 10:38:14 -0500

    Add comments and remove Wolff

--- algorithm.hpp
+++ algorithm.hpp
@@ -139,6 +139,7 @@ public:
 	}
 	void evolve_state()
 	{
+		// Get new random value, either continuous 0 to 1 or discrete
 		double R;
 		if(m_state->N == 1.0)
 		{
@@ -148,28 +149,35 @@ public:
 		{
 			R = randN( m_state->N )/( m_state->N );
 		}
+		// Select site to propose change
 		int i = randN(m_N);
 		double initial = (*m_state)[i];
 		double E_i = 0;
 		double E_f = 0;
 
+		// Calculate initial energy
 		for (auto it = m_couplings->begin(i); it != m_couplings->end(i); ++it)
 		{
 			int j = it->first;
 			E_i += m_state->energy(i, j);
 		}
 
+		// Make change
 		(*m_state)[i] = R;
 
+		// Calculate new energy
 		for (auto it = m_couplings->begin(i); it != m_couplings->end(i); ++it)
 		{
 			int j = it->first;
 			E_f += m_state->energy(i, j);
 		}
+
+		// Accept change?
 		double p = exp(-1 * (m_state->B) * (E_f - E_i));
 
 		if (p < 1 && (1.0 - p) > rand0_1())
 		{
+			// Reject change, back to initial
 			(*m_state)[i] = initial;
 		}
 	}
@@ -193,81 +201,4 @@ private:
 	int m_N;
 	int m_w;
 };
-// Note the Wolff algorithm doesn't work for this physical model and isn't properly implemented
-// template <class Couplings>
-// class Wolff
-// {
-// public:
-// 	Wolff(Couplings *t_couplings)
-// 		: m_couplings( t_couplings )
-// 	{}
-// 	void evolve_state()
-// 	{
-// 		double R = rand0_1();
-// 		int i = randN(m_N);
-//
-// 		(*m_state)[i] = R;
-// 		propigate(i);
-// 		m_traversed.clear();
-// 	}
-//
-// 	void setState(State <Wolff> *t_state)
-// 	{
-// 		m_state = t_state;
-// 		m_N = m_state->size();
-// 		m_w = std::sqrt(m_N);
-// 	}
-//
-// private:
-// 	State <Wolff> *m_state;
-// 	Couplings *m_couplings;
-// 	std::vector <int> m_traversed;
-// 	int m_N;
-// 	int m_w;
-// 	bool in_m_traversed(int i)
-// 	{
-// 		return std::find(m_traversed.begin(), m_traversed.end(), i) != m_traversed.end();
-// 	}
-//
-// 	void q_swap(int i, int j)
-// 	{
-// 		double e1 = m_state->energy(i, j);
-// 		double init = (*m_state)[j];
-//
-// 		(*m_state)[j] = (*m_state)[i];
-// 		if ((1 - exp((m_state->B) * (m_state->energy(i, j) - e1))) > rand0_1())
-// 		{
-// 			propigate(j);
-// 		}
-// 		else
-// 		{
-// 			(*m_state)[j] = init;
-// 		}
-// 	}
-//
-// 	void propigate(int i)
-// 	{
-// 		for (auto it = m_couplings->begin(i); it != m_couplings->end(i); ++it)
-// 		{
-// 			int j = it->first;
-// 			if (!(in_m_traversed(j)))
-// 			{
-// 				m_traversed.push_back(j);
-// 				q_swap(i, j);
-// 			}
-// 		}
-// 	}
-// 	double total_energy()
-// 	{
-// 		double E = 0;
-// 		for(int i = 0; i < m_N; i++)
-// 		{
-// 			for (auto it = m_couplings->begin(i); it != m_couplings->end(i); ++it)
-// 			{
-// 				E += m_state->energy(i, it->first);
-// 			}
-// 		}
-// 		return E;
-// 	}
-// };
 #endif
