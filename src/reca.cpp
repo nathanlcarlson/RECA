c547062e50825af62250b8c8b9b10dd267b77d95
Nathan Carlson <carlsonfatcat0@gmail.com>
2017-10-05 15:58:04 -0500

    Clean up and write our energies

--- src/reca.cpp
+++ src/reca.cpp
@@ -63,74 +63,116 @@ int main(int argc, char **argv) {
 	auto my_metro = std::unique_ptr<Metropolis<Bonds>>(new Metropolis<Bonds>( my_state, A ));
 	auto control_metro = std::unique_ptr<Metropolis<Bonds>>(new Metropolis<Bonds>( control_state, A ));
 
+
+	double freq = atof(argv[4])/100.0;
+
 	int min_dt = 0;
 	int max_dt = atoi(argv[2]);
 	int n_dt = atoi(argv[3]);
+
+	std::ofstream corfile;
+	std::ofstream enrfile;
+	std::string filename = argv[1];
+	corfile.open(filename);
+	enrfile.open(filename+".enr");
+
+	corfile << "#Max dt " << max_dt
+				  << ", N dt " << n_dt
+					<< ", freq RECA " << freq
+					<< "\n";
+
+
+
 	int t = 0;
-	double freq = atof(argv[4])/100.0;
-	double s;
-	double s_t;
-	double s_dt;
+	int t_start = 1000;
 
   double err;
 	double control;
 	double expr;
+
 	std::vector<double> sum_dts;
 	std::vector<int> n_dts;
 	n_dts.resize(max_dt);
 	sum_dts.resize(max_dt);
 	std::fill(sum_dts.begin(), sum_dts.end(), 0);
 	std::fill(n_dts.begin(), n_dts.end(), 0);
-	//std::cout << argv[0] << '\n';
-	std::ofstream outfile;
-	outfile.open(argv[1]);
 
-	std::ofstream errfile;
-	std::string filename = argv[1];
-	errfile.open(filename+".err");
 
-	outfile << "#Max dt " << max_dt
-				  << ", N dt " << n_dt
-					<< ", freq RECA " << freq
-					<< "\n";
-	while (true) {
-		// Save current t step in states history
-		my_state->save();
-		// This for loop is a beast
-		for(int dt=min_dt; (dt <= t && dt < max_dt); dt++) {
- 			s = 0;
- 			for( int j=0; j < n_nodes; j++ ) {
- 				s_t = my_state->get_history(t - dt , j);
- 				s_dt = my_state->get_history(t , j);
- 				s += cos(s_t*2*M_PI) * cos(s_dt*2*M_PI) + sin(s_t*2*M_PI) * sin(s_dt*2*M_PI);
- 			}
- 			n_dts[dt]++;
- 			sum_dts[dt] += s;
- 			if(n_dts[dt] == n_dt) {
- 				min_dt = dt + 1;
- 				if(min_dt == max_dt + 1) {
- 					for (auto it=sum_dts.begin(); it!=sum_dts.end(); ++it)
-						outfile << *it/(n_dt*n_nodes) << '\n';
-					outfile.close();
-					errfile.close();
-				 	return 0;
-   			}
-   		}
-   	}
-		// Evovle using RECA at freq
+	// Evolve system without gathering any metrics
+	while (t < t_start) {
+
+		// Evolve mixed
 		if(rand0_1() < freq){
 			my_reca->evolve_state();
 		}
 		else {
 			my_metro->evolve_state();
 		}
+
 		// Evolve control state
 		control_metro->evolve_state();
-		// Calculate error to control state
+		t++;
+	}
+
+	// Reset time
+	t = 0;
+
+	// Gather metrics
+	while (true) {
+
+		// Save current t step in states history
+		my_state->save();
+
+		// This for loop is a beast
+		for(int dt=min_dt; (dt <= t && dt < max_dt); dt++) {
+
+ 			double s = 0;
+
+ 			for( int j=0; j < n_nodes; j++ ) {
+
+ 				double s_t = my_state->get_history(t - dt , j);
+ 				double s_dt = my_state->get_history(t , j);
+ 				s += cos(s_t*2*M_PI) * cos(s_dt*2*M_PI) + sin(s_t*2*M_PI) * sin(s_dt*2*M_PI);
+
+ 			}
+
+ 			n_dts[dt]++;
+ 			sum_dts[dt] += s;
+
+ 			if(n_dts[dt] == n_dt) {
+
+ 				min_dt = dt + 1;
+
+ 				if(min_dt == max_dt + 1) {
+
+ 					for (auto it=sum_dts.begin(); it!=sum_dts.end(); ++it)
+						corfile << *it/(n_dt*n_nodes) << '\n';
+
+					corfile.close();
+					enrfile.close();
+				 	return 0;
+
+   			}
+   		}
+   	}
+
+		// Evovle mixed
+		if(rand0_1() < freq){
+			my_reca->evolve_state();
+		}
+		else {
+			my_metro->evolve_state();
+		}
+
+		// Evolve control state
+		control_metro->evolve_state();
+
+		// Get energies
 		control = control_metro->total_energy();
 		expr = my_metro->total_energy();
-		err =  fabs(control - expr)/fabs(control);
-		errfile << err << '\n';
+
+		enrfile << control << ' ' << expr <<'\n';
+
 		// Advance time
 		t++;
 
