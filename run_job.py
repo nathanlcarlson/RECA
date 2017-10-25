af50381b4b8a11f296ea61c62a3d79e0e747eb27
Nathan Carlson <nathan.l.carlson@und.edu>
2017-10-25 12:49:50 -0500

    Batch run jobs using Python

--- run_job.py
+++ run_job.py
@@ -16,21 +16,35 @@ def main():
                         help='')
     args = parser.parse_args()
     
+    m_dict = {}
     for L in args.width:
+      m_dict[L] = {}
       for beta in args.betas:
+        m_dict[L][beta] = {}
         for freq in args.freqs:
+          m_dict[L][beta][freq] = {}
+          m_dict[L][beta][freq]['files'] = {}
+          m_dict[L][beta][freq]['files']['energy'] = []
+          m_dict[L][beta][freq]['files']['conf'] = []
           for c in range(args.samples):
+            
             path = args.root+"/L"+L+"/beta"+beta+"/freq"+freq
-            print path+"/"+str(c)
             mkdirs(path)
+            path = path+"/"+str(c)
+            print path
+            m_dict[L][beta][freq]['files']['conf'].append(path+".conf")
+            m_dict[L][beta][freq]['files']['energy'].append(path+".enr")
             os.system("./reca " + 
-                       path+"/"+str(c) + " " +
+                       path + " " +
                        L + " " +
                        beta + " " +
                        freq + " " +
                        args.steps
                      )
-        
+
+          #os.system("python proc/energy.py -c "+ " ".join(controls) + " -e " + " ".join(expr))
+          
+          
 def mkdirs(path):
   try:
     os.makedirs(path)
