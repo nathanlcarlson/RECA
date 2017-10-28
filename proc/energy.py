054378c7bce066ff93ee351873f970f12e3df66a
Nathan Carlson <nathan.l.carlson@und.edu>
2017-10-28 16:44:16 -0500

    Add plotting of energy autocorrelation

--- proc/energy.py
+++ proc/energy.py
@@ -1,30 +1,60 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import argparse
+import pylab as plt
 def main():
   parser = argparse.ArgumentParser(description='')
   parser.add_argument('-c','--control', dest='control', nargs='+',
                       help='', required = True)
   parser.add_argument('-e','--energy', dest='energy', nargs='+',
                       help='', required = True)
-  
+  parser.add_argument('-a','--auto', dest='auto', action = 'store_true',
+                      help='')
+  parser.add_argument('-i','--ignore', dest='ignore', type = int , default = 0,
+                      help='Ignore last N lines')
   args = parser.parse_args()
-  
-  control = avg_energy(args.control)
-  expr = avg_energy(args.energy)
-  
-  controlhandles, = plt.plot(control, label = "Control")
-  exprhandles, = plt.plot(expr, label = "Expiermental")
-  plt.legend( [controlhandles, exprhandles], ["Control", "Experimental"] )
+
+  expr = []
+  for sim in args.energy:
+      expr.append( np.genfromtxt( sim, skip_footer = args.ignore ) )
+  control = []
+  for sim in args.control:
+      control.append( np.genfromtxt( sim, skip_footer = args.ignore ) )
+
+  auto_expr = []
+  for data in expr:
+      auto_expr.append( autocorrelate( data ) )
+  auto_control = []
+  for data in control:
+      auto_control.append( autocorrelate( data ) )
+
+
+  plot_all(expr, "Experimental", 1)
+  plot_all(control, "Control")
+  plt.legend( loc = 'upper right' )
   plt.show()
-  
-def avg_energy(data_files):
-  samples = []
-  for sim in data_files:
-    samples.append(np.genfromtxt(sim))
-    
-  out = np.mean(samples, axis=0)
-  print out.shape
-  return out
-  
+
+  plot_all(auto_expr, "Experimental", 2)
+  plot_all(auto_control, "Control")
+  plt.legend( loc = 'upper right' )
+  plt.show()
+
+def plot_all(dset, label):
+
+  i = 0
+  plt.subplot()
+  for data in dset:
+    plt.plot(data, label = label+" "+str(i))
+    i+=1
+
+def avg_energy(data):
+
+  return np.mean(data, axis = 0)
+
+def autocorrelate(data):
+
+  data = np.mean(data) - data
+  result = np.correlate(data, data, mode='full')
+  return result[result.size/2:]
+
 main()
