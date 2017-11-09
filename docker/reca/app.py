f5a0bd2e685a85acf652fcda199d058509b5551c
Nathan Carlson <nathan.l.carlson@und.edu>
2017-11-08 20:44:09 -0600

    Test displaying graphs in webapp

--- docker/reca/app.py
+++ docker/reca/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, make_response
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 import os
 import socket
@@ -34,6 +34,36 @@ def hello():
 
     return render_template('hello.html', form=form)
 
+@app.route("/simple.png")
+def simple():
+    import datetime
+    import StringIO
+    import random
+
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    from matplotlib.figure import Figure
+    from matplotlib.dates import DateFormatter
+
+    fig=Figure()
+    ax=fig.add_subplot(111)
+    x=[]
+    y=[]
+    now=datetime.datetime.now()
+    delta=datetime.timedelta(days=1)
+    for i in range(10):
+        x.append(now)
+        now+=delta
+        y.append(random.randint(0, 1000))
+    ax.plot_date(x, y, '-')
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+    fig.autofmt_xdate()
+    canvas=FigureCanvas(fig)
+    png_output = StringIO.StringIO()
+    canvas.print_png(png_output)
+    response=make_response(png_output.getvalue())
+    response.headers['Content-Type'] = 'image/png'
+
+return response
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
 
