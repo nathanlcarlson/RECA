18e76125f7f3b0ac385f7e71b03e96204887b854
Nathan Carlson <ncarlson@iteris.com>
2017-12-13 14:22:26 -0600

    Implement PyYAML for configuration

--- run_job.py
+++ run_job.py
@@ -2,45 +2,50 @@
 import argparse
 import os
 import subprocess
+import yaml
+
 def main():
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('-b','--beta', dest='betas', nargs='+',
-                        help='Inverse temperature')
-    parser.add_argument('-t','--steps', dest='steps', default = str(pow(2,15)),
-                        help='Number of Monte Carlo steps in a simulation')
-    parser.add_argument('-f','--freq', dest='freqs', nargs='+',
-                        help='Percentage of RECA moves to use, 0 - 100')
-    parser.add_argument('-L','--width', dest='width', nargs='+',
-                        help='The width of the lattice to simulate')
-    parser.add_argument('-n','--samples', dest='samples', type=int, default = 1,
-                        help='Number of simulations to do for each unique parameter set')
-    parser.add_argument('-r','--root', dest='root', default = "data",
-                        help='The root directory to store data in')
+    parser.add_argument('-f','--yaml', dest='yaml', required = True,
+                        help='YAML config file')
     args = parser.parse_args()
 
-    m_dict = {}
-    for L in args.width:
-      m_dict[L] = {}
-      for beta in args.betas:
-        m_dict[L][beta] = {}
-        for freq in args.freqs:
-          m_dict[L][beta][freq] = {}
-          m_dict[L][beta][freq]['process'] = []
-          for c in range(args.samples):
+    with open(args.yaml) as f:
+      yargs = yaml.safe_load(f)
 
-            mkdirs(args.root+"/L"+L+"/Beta"+beta+"/Freq"+freq)
-            path = args.root+"/L"+L+"/Beta"+beta+"/Freq"+freq+"/"+str(c)
+    ydefaults = {'beta': [1], 'width': [10], 'percent_RECA': [5], 'samples': 5, 'root': 'data', 'steps': pow(2, 15)}
 
-            m_dict[L][beta][freq]['process'].append(subprocess.Popen(["./reca",
-                                                                  L,
-                                                                  beta,
-                                                                  freq,
-                                                                  args.steps,
-                                                                  path
-                                                                 ],
-                                                                  stdout=subprocess.PIPE,
-                                                                  stderr=subprocess.STDOUT))
+    yargs = set_defaults(yargs, ydefaults)
 
+    for L in yargs['width']:
+      for beta in yargs['beta']:
+        for freq in yargs['percent_RECA']:
+          for c in range(yargs['samples']):
+
+            L = str(L)
+            beta = str(beta)
+            freq = str(freq)
+
+            mkdirs(yargs['root']+"/L"+L+"/Beta"+beta+"/Freq"+freq)
+            path = yargs['root']+"/L"+L+"/Beta"+beta+"/Freq"+freq+"/"+str(c)
+
+            subprocess.Popen(["./reca",
+                              L,
+                              beta,
+                              freq,
+                              str(yargs['steps']),
+                              path
+                              ],
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.STDOUT)
+
+def set_defaults(yargs, ydefaults):
+
+  for key in ydefaults.keys():
+    if key not in yargs.keys():
+      yargs[key] = ydefaults[key]
+
+  return yargs
 
 def mkdirs(path):
   try:
