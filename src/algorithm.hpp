50430b91efba411c2e65458058e41b7f389aa037
Nathan Carlson <nathan.l.carlson@und.edu>
2018-02-07 16:09:09 -0600

    Get rid of multiple replica support

--- src/algorithm.hpp
+++ src/algorithm.hpp
@@ -56,28 +56,26 @@ class RECA {
 	private:
 		double m_R;
 		int m_L;
-		int m_k = 15;
     int m_step = 0;
 
 		std::shared_ptr<State> m_state;
-		std::vector<std::shared_ptr<State>> m_replica;
+		std::shared_ptr<State> m_replica;
 		std::unique_ptr<Cluster> m_cluster;
 
 		// TODO Implement this optimization
 		void crotate_and_exchange(int i, std::shared_ptr<State> S_i, std::shared_ptr<State> S_j) {
-		
+
 			S_i->shift_one(i, -1*m_R);
 			S_j->shift_one(i, m_R);
 
 			double tmp = (*S_j)[i];
 			(*S_j)[i] = (*S_i)[i];
 			(*S_i)[i] = tmp;
-		
+
 		}
 
 		// Check energy change and add to cluster, or not
 		void q_swap(int i, int j, std::shared_ptr<State> S_i, std::shared_ptr<State> S_j) {
-
 			double U_i = (*S_i)[j];
 			double R_i = (*S_j)[j];
 
@@ -102,95 +100,38 @@ class RECA {
 
 		// Recursively called to grow cluster
 		void propigate(int i, std::shared_ptr<State> S_i, std::shared_ptr<State> S_j) {
-
 			for (auto neighbor = S_i->bonds()->begin(i); neighbor != S_i->bonds()->end(i); ++neighbor) {
-
 				int j = *neighbor;
-
 				if (!(m_cluster->contains(j))) {
-
 					q_swap(i, j, S_i, S_j);
-
 				}
 			}
 		}
 
 	public:
-		RECA(std::shared_ptr<State> t_state, int t_replicas)
-			: m_state(t_state), m_k(t_replicas)
+		RECA(std::shared_ptr<State> t_state)
+			: m_state(t_state)
 		{
 			m_L = m_state->size();
-			for(int i = 0; i < m_k; ++i){
-				m_replica.push_back(std::make_shared<State>( *m_state ));
-				m_replica[i]->randomize_all();
-			}
+
+			m_replica = std::make_shared<State>( *m_state );
+			m_replica->randomize_all();
+
 			m_cluster = std::make_unique<Cluster>( m_L );
 		}
 		void evolve_state() {
 
-			std::vector<int> replica_ids;
-			for(int i = 0; i < m_k; ++i){
-				replica_ids.push_back(i);
-			}
-
-			// Make random pairs between replicas
-			while(replica_ids.size() != 1){
-				// Set rotation
-				m_R = m_state->get_shift();
-				
-				int m = replica_ids[randN(replica_ids.size())];
-
-				auto it = std::find(replica_ids.begin(), replica_ids.end(), m);
-				if(it != replica_ids.end())
-				    replica_ids.erase(it);
-
-				int n = replica_ids[randN(replica_ids.size())];
-
-				it = std::find(replica_ids.begin(), replica_ids.end(), n);
-				if(it != replica_ids.end())
-				    replica_ids.erase(it);
-
-				int i = randN(m_L);
-				crotate_and_exchange(i, m_replica[m], m_replica[n]);
-				m_cluster->add(i);
-
-				propigate(i, m_replica[m], m_replica[n]);
-				m_cluster->clear();
-
-			}
-
 			// Set rotation
 			m_R = m_state->get_shift();
-			
+
 			// Seed site
 			int i = randN(m_L);
-			crotate_and_exchange(i, m_replica[replica_ids[0]], m_state);
+			crotate_and_exchange(i, m_replica, m_state);
 			m_cluster->add(i);
 
 			// Propigate from seed site, building the cluster
-			propigate(i, m_replica[replica_ids[0]], m_state);
+			propigate(i, m_replica, m_state);
 			m_cluster->clear();
-
-		}
-		std::shared_ptr<State> get_replica(int i){
-			return m_replica[i];
-		}
-		double total_energy() {
-
-			double E = 0;
-
-			for(int i = 0; i < m_L; i++) {
-
-				for (auto neighbor = m_state->bonds()->begin(i); neighbor != m_state->bonds()->end(i); ++neighbor) {
-
-					if( i < *neighbor) {
-
-						E += m_state->energy(i, *neighbor);// + m_replica->energy(i, *neighbor);
-					}
-				}
-			}
-
-			return E;
 
 		}
 		int step() {
