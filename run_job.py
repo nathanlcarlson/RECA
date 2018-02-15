b4d001b023ee84ba6068ad1832200bc6342bdcf2
Nathan Carlson <nathan.l.carlson@und.edu>
2018-02-14 18:31:28 -0600

    Change def of MC step, change from pri and rep to state pool, and add param to control Metro's actions

--- run_job.py
+++ run_job.py
@@ -14,32 +14,45 @@ def main():
     with open(args.yaml) as f:
       yargs = yaml.safe_load(f)
 
-    ydefaults = {'beta': [1], 'width': [10], 'percent_RECA': [5], 'samples': 5, 'root': 'data', 'steps': pow(2, 15)}
+    ydefaults = {'beta': [1],
+                'width': [10],
+                'percent_RECA': [5],
+                'samples': 5,
+                'root': 'data',
+                'pri_or_all': 0,
+                'seed': str(randint(0,pow(2,20))),
+                'n_states': [2]
+                }
 
     yargs = set_defaults(yargs, ydefaults)
 
-    for L in yargs['width']:
-      for beta in yargs['beta']:
-        for freq in yargs['percent_RECA']:
-          for c in range(yargs['samples']):
+    for N in yargs['n_states']:
+        for L in yargs['width']:
+          for beta in yargs['beta']:
+            for freq in yargs['percent_RECA']:
+              for c in range(yargs['samples']):
 
-            L = str(L)
-            beta = str(beta)
-            freq = str(freq)
+                L = str(L)
+                beta = str(beta)
+                freq = str(freq)
+                N = str(N)
+                S = str(yargs['pri_or_all'])
 
-            mkdirs(yargs['root']+"/L"+L+"/Beta"+beta+"/Freq"+freq)
-            path = yargs['root']+"/L"+L+"/Beta"+beta+"/Freq"+freq+"/"+str(c)
+                mkdirs(yargs['root']+"/N"+N+"/L"+L+"/Beta"+beta+"/Freq"+freq)
+                path = yargs['root']+"/N"+N+"/L"+L+"/Beta"+beta+"/Freq"+freq+"/"+str(c)
 
-            subprocess.Popen(["./RECA",
-                              L,
-                              beta,
-                              freq,
-                              str(yargs['steps']),
-                              path,
-                              str(randint(0,pow(2,20)))
-                              ],
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.STDOUT)
+                print "./RECA",L,beta,freq,S,path,yargs['seed'],N
+                subprocess.Popen(["./RECA",
+                                  L,
+                                  beta,
+                                  freq,
+                                  S,
+                                  path,
+                                  yargs['seed'],
+                                  N
+                                  ],
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.STDOUT)
 
 def set_defaults(yargs, ydefaults):
 
