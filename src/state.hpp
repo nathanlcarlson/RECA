30589d9ab4d47b0875a088977a53d8fe7b40379f
Nathan Carlson <nathan.l.carlson@und.edu>
2018-02-26 15:04:52 -0600

    Remove unused history feature

--- src/state.hpp
+++ src/state.hpp
@@ -24,8 +24,6 @@ class State {
 		std::map <char, std::shared_ptr<Bonds>> m_bonds_map;
 		std::shared_ptr <Node> m_node;
 		std::vector <double> m_v;
-		std::vector <double> m_energy_history;
-		std::vector <std::vector <double>> m_history;
 
 	public:
 
@@ -39,20 +37,16 @@ class State {
 					std::shared_ptr<Node> t_node)
 			: B(t_B), m_energy(t_f), m_L(t_L), m_bonds(t_bonds), m_node(t_node)
 		{
-
-			for(   auto& b: t_bonds ) {
+			for(auto& b: t_bonds ) {
 				m_bonds_map[b->id] = b;
 			}
 			m_v.resize(t_L);
 			randomize_all();
-
 		}
 
 		// Calculate energy between i and j
-		double energy(  int i,   int j) {
-
+		double energy(int i, int j) {
 			return m_energy(this, i, j);
-
 		}
 
 		// Shift all with help of Node class
@@ -66,18 +60,22 @@ class State {
 		void shift_one(int i, double _s) {
 			m_v[i] = m_node->shifted_value(m_v[i], _s);
 		}
-
+		void shift_one(int i) {
+			m_v[i] = m_node->shifted_value(m_v[i]);
+		}
+		void shift_one() {
+			shift_one(randN(m_L));
+		}
+		// Generate shift value to use later
 		double get_shift() {
 			return m_node->get_shift();
 		}
 
 		// Randomizes the whole state with help of Node class
 		void randomize_all() {
-
 			for(int i = 0; i < m_L; ++i) {
 				m_v[i] = m_node->random_value();
 			}
-
 		}
 
 		// Randomizes one with help of Node class
@@ -94,83 +92,44 @@ class State {
 			return m_node->getRGB(m_v[i], _rgb_out);
 		}
 
-		// TODO Replace this system for saving data with a more modular solution
-		void save() {
-
-			m_history.push_back(m_v);
-			m_energy_history.push_back(total_energy());
-
-		}
-
 		void print() {
-
 			std::cout << "State contains:";
 			for ( auto& site: m_v) {
 				std::cout << ' ' << site;
 			}
 			std::cout << '\n';
-
 		}
 
 		double&operator[](int i) {
-
 			return m_v[i];
-
 		}
 
 		// Get bonds by ID
 		std::shared_ptr<Bonds> bonds(char id) {
-
 			return m_bonds_map[id];
-
 		}
 
 		// Get first bond in list
 		std::shared_ptr<Bonds> bonds() {
-
 			return m_bonds[0];
-
 		}
 
 		// Calculate total energy
 		double total_energy() {
-
 			double E = 0;
-
 			for(int i = 0; i < m_L; i++) {
-
 				for (auto neighbor = m_bonds[0]->begin(i); neighbor != m_bonds[0]->end(i); ++neighbor) {
-
 					if( i < *neighbor) {
-
 						E += energy(i, *neighbor);
 					}
 				}
 			}
-
 			return E;
-
-		}
-		// TODO Replace this system for saving data with a more modular solution
-		std::vector<std::vector<double>> history(){
-
-			return m_history;
-		}
-		// TODO Replace this system for saving data with a more modular solution
-		std::vector<double>& energy_history(){
-
-			return m_energy_history;
 		}
 
 		int size() {
-
 			return m_v.size();
-
 		}
 
-		int steps() {
-
-			return m_history.size();
-		}
 };
 #endif
